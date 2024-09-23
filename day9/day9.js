a64ddaef69a5eade0f44929a02242552619a8ac8
2notprime <long3172004@gmail.com>
2024-09-24 00:10:25 +0700

    day9

---
+++ day9/day9.js
@@ -0,0 +1,14 @@
+// Array
+console.log("Array")
+
+let a = ["MU", "MC", "Bar"]
+a.push("VN")
+console.log("type of a:", typeof a, a)
+
+let infor = [
+    {name: "Jane", age: 25},
+    {name: "John", age: 30},
+    {name: "Emily", age: 28}
+]
+
+console.log("type of infor:", typeof infor, infor)
