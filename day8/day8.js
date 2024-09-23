76f733165cf904dfb01d41050b273a06b122e720
2notprime <long3172004@gmail.com>
2024-09-23 23:09:49 +0700

    day8

---
+++ day8/day8.js
@@ -0,0 +1,11 @@
+// Object
+console.log("Object")
+
+let a = {
+    name: "Alice",
+    age: 25
+};
+
+console.log(`type of a is: ${typeof a}`);
+console.log(`a.name is: ${a.name} or ${a[`name`]} 
+a.age is: ${a.age} or ${a[`age`]}`); 
